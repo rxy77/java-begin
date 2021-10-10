3859bdc3097d1c521277ec862b2acd666315539a
rxy77 <547602265@qq.com>
2021-10-10 11:01:53 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package com.rxy.hello;
+
+/**
+ * Created with IntelliJ IDEA
+ * Description:
+ * User: yupei.wang
+ * Date: 2021-04-25
+ * Time: 16:41
+ */
+public class HelloWorld {
+
+    public static void main(String args[]) {
+        System.out.println("com/rxy/hello");
+    }
+}
